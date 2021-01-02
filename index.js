173a8d4b9377b97aa62e741a4962b0a02be0e537
git <vhulf@Emmas-MacBook-Pro.local>
2021-01-02 01:39:51 -0600

    beeg update

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+let express = require('express');
+let app = express();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+const fs = require('fs');
+const { forEach } = require('methods');
+const path = require('path');
+const util = require('util');
+
+const readDir = util.promisify(fs.readdir);
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/bubble', (req, res) => {
-    res.sendFile(__dirname + 'bubble/bubble.html');
-  });
+  res.sendFile(__dirname + '/bubble/bubble.html');
+});
+
+app.get('/bubbPanel', async (req, res) => {
+  var imageFileList = [];
+  let files = await readDir('public/images/');
+
+  res.render('bubbPanel', {images: files});
+});
 
 io.on('connection', (socket) => {
-    io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
     socket.on('chat', (msg) => {
         io.emit('chat message', msg);
+        if (msg == "lol") {
+          io.emit('bubble', 'images/webex-lolxd.gif');
+        } else if (msg == "astro") {
+          io.emit('bubble', 'images/astro.jpg');
+        }
+    });
+
+    socket.on('bubbPlz', (bub) => {
+      io.emit('bubble', bub);
     });
   });
 
